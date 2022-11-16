be2ffc03ccca7beaa4d4f9e2f4b4d0a9c6409db3
Bernardina <72995149+bmaldonado79@users.noreply.github.com>
2022-11-16 07:56:05 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bernardina Maldonado on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
